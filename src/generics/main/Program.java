eaa8f8f960b65191ca93a3d9878d1e48e75d6f56
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-07-02 18:28:39 -0300

    Genéricos delimitados

--- src/generics/main/Program.java
+++ src/generics/main/Program.java
@@ -1,26 +1,38 @@
 package generics.main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-import generics.service.PrintService;
+import generics.entitys.Product;
+import generics.service.CalculationService;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		PrintService<Integer> ps = new PrintService<>();
+		List<Product> list = new ArrayList<>();
 
-		System.out.println("How many values? ");
-		Integer n = sc.nextInt();
+		String path = "C://temp//in.txt";
 
-		for (int i = 0; i < n; i++) {
-			Integer value = sc.nextInt();
-			ps.addValues(value);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+
+			Product x = CalculationService.max(list);
+			System.out.println("Max:");
+			System.out.println(x);
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
-		
-		ps.print();
-		System.out.print("First: " + ps.first());
-		sc.close();
 	}
 }
